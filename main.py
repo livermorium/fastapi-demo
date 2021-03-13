c88d7890e67432d0c91fd0019febbce892f23c01
chazzhuang(黄长志) <chazzhuang@tencent.com>
2021-03-13 20:22:29 +0800

    put, data validate

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from typing import Optional
+
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
 
 
 @app.get("/")
@@ -12,3 +22,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"id": item_id, "name": item.name}
