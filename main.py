4f162fff6dd6175b10cd802f73db55194db17bad
chazzhuang <chazzhuang@tencent.com>
2021-03-14 19:27:40 +0800

    path parameter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from enum import Enum
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -14,10 +14,21 @@ class Item(BaseModel):
     is_offer: Optional[bool] = None
 
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 @app.get("/")
 def read_root():
     """/"""
     return {"hello": "world"}
+
+
+@app.get("/items/me")
+async def read_item_me():
+    return {"id": "the current id"}
 
 
 @app.get("/items/{item_id}")
@@ -28,3 +39,13 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"id": item_id, "name": item.name, "price": item.price}
+
+
+@app.get("/models/{model_name}")
+def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
